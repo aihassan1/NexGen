0e81b4b844df91a08ce420451a8a20fa84903b23
aihassan1 <abdosruor57@gmail.com>
2024-03-22 23:33:52 +0200

    devoloped about page

--- flask/NexGen.py
+++ flask/NexGen.py
@@ -6,17 +6,20 @@ app = Flask(__name__)
 @app.route("/home")
 @app.route("/")
 def index():
-    return render_template("base.html")
+    return render_template("index.html")
 
 
+@app.route("/about")
 def about():
     return render_template("about.html")
 
 
+@app.route("/contact")
 def contact():
     return render_template("contact.html")
 
 
+@app.route("/services")
 def services():
     return render_template("services.html")
 
